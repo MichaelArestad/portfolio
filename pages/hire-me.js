58ff7fc2b9910325a90f124383b486ac79249f26
Michael Arestad <marestad@gmail.com>
2021-02-16 13:09:03 -0700

    Updated hire me page content

--- pages/hire-me.js
+++ pages/hire-me.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import SEO from "../components/seo";
 import Layout from "../components/layout/layout";
+import Hero from "../components/hero/hero";
+import Back from "../components/back/back";
+import Card from "../components/card/card";
 import List from "../components/list/list";
 
 export default function Home() {
@@ -9,23 +12,29 @@ export default function Home() {
 			<SEO title="say hi" />
 
 			<main>
-				<h2>hiRe mE</h2>
-				<List minimal>
-					<li>
-						<a href="mailto:marestad@gmail.com">Email</a>
-					</li>
-				</List>
-				<List minimal>
-					<li>
-						<a href="https://michael.blog/">Blog</a>
-					</li>
-					<li>
-						<a href="https://github.com/MichaelArestad">GitHub</a>
-					</li>
-					<li>
-						<a href="https://twitter.com/michaelarestad">Twitter</a>
-					</li>
-				</List>
+				<Hero
+					title="Michael Arestad"
+					description="I'm currently accepting new clients. If you are interested in working together, please reach out."
+					bg="#fff"
+					textColor="#000">
+					<Back href="/">Home</Back>
+				</Hero>
+				<Card>
+					<List minimal condensed>
+						<li>
+							<a href="mailto:marestad@gmail.com">Email</a>
+						</li>
+						<li>
+							<a href="https://michael.blog/">Blog</a>
+						</li>
+						<li>
+							<a href="https://github.com/MichaelArestad">GitHub</a>
+						</li>
+						<li>
+							<a href="https://twitter.com/michaelarestad">Twitter</a>
+						</li>
+					</List>
+				</Card>
 			</main>
 		</Layout>
 	);
