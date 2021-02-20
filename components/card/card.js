91f384cf05cb40c579587680f932b5bc11e2bb16
Michael Arestad <marestad@gmail.com>
2021-02-20 16:25:32 -0700

    Removed unused event handlers from Card component

--- components/card/card.js
+++ components/card/card.js
@@ -6,11 +6,11 @@ export default class Card extends React.Component {
 	renderCard() {
 		if ( this.props.href ) {
 			return	(
-				<a href={ this.props.href } className={ cn( styles.cardLink, this.props.className ) } onMouseEnter={ this.props.onMouseEnter } onMouseLeave={ this.props.onMouseLeave } style={{ background: this.props.backgroundColor, color: this.props.textColor }}>{ this.props.children }</a>
+				<a href={ this.props.href } className={ cn( styles.cardLink, this.props.className ) } style={{ background: this.props.backgroundColor, color: this.props.textColor }}>{ this.props.children }</a>
 			);
 		} else {
 			return	(
-				<div onClick={ this.props.onClick } onMouseEnter={ this.props.onMouseEnter } onMouseLeave={ this.props.onMouseLeave } className={ cn( styles.card, this.props.className ) } style={{ background: this.props.backgroundColor, color: this.props.textColor }}>{ this.props.children }</div>
+				<div onClick={ this.props.onClick } className={ cn( styles.card, this.props.className ) } style={{ background: this.props.backgroundColor, color: this.props.textColor }}>{ this.props.children }</div>
 			);
 		}
 	}
