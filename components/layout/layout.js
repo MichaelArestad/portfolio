dee5aa28a4926126c381e4f50653c627304658e8
Michael Arestad <marestad@gmail.com>
2021-02-23 09:47:51 -0700

    Added decorative X

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -3,12 +3,18 @@ import PageMeta from "../page-meta";
 import cn from "classnames";
 import styles from "./layout.module.scss";
 import SourceBadge from "../sourceBadge/souce-badge";
+import { Fragment } from "react";
 
 export default function Layout({ addedStyle, backgroundColor, textColor, header, home, children }) {
 	const hasHeader = () => {
 		if ( header === true ) {
 			if( home ) {
-				return <Header home />;
+				return (
+					<Fragment>
+						<div className={ styles.x } />
+						<Header home />
+					</Fragment>
+				);
 			} else {
 				return <Header />;
 			}
