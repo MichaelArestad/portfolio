cc6b8407cb4033328e2ebf3e3fa368a8a407fc84
Michael Arestad <marestad@gmail.com>
2021-02-24 14:45:45 -0700

    Made FancyLink Fancier

--- components/fancyLink/fancy-link.js
+++ components/fancyLink/fancy-link.js
@@ -1,10 +1,49 @@
 import styles from "./fancy-link.module.scss";
 import Link from "next/link";
+import cn from "classnames";
 
-export default function FancyLink({ link, href, children }) {
-	if ( link ) {
-		return <span className={ styles.fancyLink }><Link href={ href }>{ children }</Link></span>
+export default function FancyLink({
+	link,
+	href,
+	inverted,
+	surprise,
+	children,
+}) {
+	let surpriseContent;
+	if (surprise) {
+		surpriseContent = '"' + surprise + '"';
+	}
+
+	const hasSurprise = () => {
+		if (surprise) {
+			return styles.hasSurprise;
+		}
+	};
+
+	const isInverted = () => {
+		if (inverted) {
+			return styles.isInverted;
+		}
+	};
+
+	if (link) {
+		return (
+			<span
+				className={cn(styles.fancyLink, hasSurprise(), isInverted())}
+				style={{ "--surprise-content": surpriseContent }}
+			>
+				<Link href={href}>{children}</Link>
+			</span>
+		);
 	} else {
-		return <a href={ href } className={ styles.fancyLink }>{ children }</a>
+		return (
+			<a
+				href={href}
+				className={cn(styles.fancyLink, hasSurprise(), isInverted())}
+				style={{ "--surprise-content": surprise }}
+			>
+				{children}
+			</a>
+		);
 	}
 }
