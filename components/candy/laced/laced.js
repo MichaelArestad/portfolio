7667b72af0a2b4af342ba66ce18c94deb396937b
Michael Arestad <marestad@gmail.com>
2021-03-02 13:22:57 -0700

    Changed laced to textarea

--- components/candy/laced/laced.js
+++ components/candy/laced/laced.js
@@ -2,8 +2,9 @@ import styles from "./laced.module.scss";
 
 export default function Laced({}) {
 	return (
-		<div contentEditable className={styles.laced}>
-			try out laced go ahead and edit this to create your own unique patterns
-		</div>
+		<textarea
+			className={styles.laced}
+			defaultValue="try out laced go ahead and edit this to create your own unique patterns"
+		/>
 	);
 }
