95fdb0ba5df2ed7787755f48ba099f480d7ba83f
Michael Arestad <marestad@gmail.com>
2021-03-02 07:40:04 -0700

    Updated laced styles

--- components/candy/laced/laced.js
+++ components/candy/laced/laced.js
@@ -1,12 +1,9 @@
-import Card from "../../card/card";
 import styles from "./laced.module.scss";
 
 export default function Laced({}) {
 	return (
-		<Card addedClass={styles.container}>
-			<div contentEditable className={styles.laced}>
-				try out laced go ahead and edit this to create your own unique patterns
-			</div>
-		</Card>
+		<div contentEditable className={styles.laced}>
+			try out laced go ahead and edit this to create your own unique patterns
+		</div>
 	);
 }
