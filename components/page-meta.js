4123f1e5f2466ca52c06741681df5cf87d0bed31
Michael Arestad <marestad@gmail.com>
2021-03-02 13:16:13 -0700

    FAVICONS

--- components/page-meta.js
+++ components/page-meta.js
@@ -3,9 +3,28 @@ import Head from "next/head";
 export default function PageMeta() {
 	return (
 		<Head>
-			<link rel="shortcut icon" href="/icons/icon-57x57.png" />
-			{/* <link rel="manifest" href="/manifest.webmanifest" /> */}
-			{/* <meta name="theme-color" content="#ffa7c4" /> */}
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/apple-touch-icon.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/favicon-16x16.png"
+			/>
+			<link rel="manifest" href="/site.webmanifest" />
+			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+			<meta name="msapplication-TileColor" content="#000000" />
+			<meta name="theme-color" content="#ffffff" />
+			<link rel="icon" type="image/svg+xml" href="/m.svg" />
 		</Head>
 	);
 }
