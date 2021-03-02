caf8350f989cee419c69dcb46e89be9027ced458
Michael Arestad <marestad@gmail.com>
2021-03-02 08:07:38 -0700

    Added WcDen component to EyeCandy

--- pages/projects/eye-candy.js
+++ pages/projects/eye-candy.js
@@ -1,15 +1,16 @@
-import SEO from "../../components/seo";
-import Card from "../../components/card/card";
-import Layout from "../../components/layout/layout";
 import Back from "../../components/back/back";
+import Card from "../../components/card/card";
 import Hero from "../../components/hero/hero";
+import Layout from "../../components/layout/layout";
+import SEO from "../../components/seo";
 import styles from "../../styles/eye-candy.module.scss";
 
 import CheckboxGrid from "../../components/candy/checkbox-grid/checkbox-grid";
 import DarkNet from "../../components/candy/darknet/darknet";
-import VaultPress from "../../components/candy/vaultpress/vaultpress";
-import TheVoid from "../../components/candy/the-void/the-void";
 import Laced from "../../components/candy/laced/laced";
+import TheVoid from "../../components/candy/the-void/the-void";
+import VaultPress from "../../components/candy/vaultpress/vaultpress";
+import WcDen from "../../components/candy/wcden/wcden";
 
 export default function Home() {
 	return (
@@ -25,7 +26,7 @@ export default function Home() {
 					title="Eye candy"
 					description="a design and code playground"
 					zag="big"
-					backgroundColor="#ff0054"
+					backgroundColor="#FF5254"
 					textColor="#fff"
 				>
 					<Back href="/projects/">Projects</Back>
@@ -34,9 +35,12 @@ export default function Home() {
 				<Card addedClass={styles.eyeCandy}>
 					<VaultPress />
 					<Laced />
+					<WcDen />
 					<CheckboxGrid />
 					<DarkNet />
 					<TheVoid />
+					<WcDen icon="speaker" />
+					<WcDen icon="heart" />
 				</Card>
 			</main>
 		</Layout>
