bc5c4cba3d4b50f75b1b480f7a17d469192adf5a
Michael Arestad <marestad@gmail.com>
2021-03-09 10:28:49 -0700

    add root font size and update to list component

--- components/list/list.js
+++ components/list/list.js
@@ -1,28 +1,35 @@
 import cn from "classnames";
 import styles from "./list.module.scss";
 
-export default function List({ title, className, minimal, mono, condensed, align, children }) {
+export default function List({
+	title,
+	className,
+	minimal,
+	mono,
+	condensed,
+	align,
+	children,
+}) {
 	const isMinimal = () => {
-		if ( minimal ) {
+		if (minimal) {
 			return styles.minimal;
 		}
 	};
 
 	const isCondensed = () => {
-		if ( condensed ) {
+		if (condensed) {
 			return styles.condensed;
 		}
 	};
 
 	const isMono = () => {
-		if ( mono ) {
+		if (mono) {
 			return styles.mono;
 		}
 	};
 
-
 	const alignment = () => {
-		switch ( align ) {
+		switch (align) {
 			case "center":
 				return styles.center;
 			case "right":
@@ -30,16 +37,39 @@ export default function List({ title, className, minimal, mono, condensed, align
 			default:
 				break;
 		}
-	}
+	};
 
-	if ( title ) {
+	if (title) {
 		return (
-			<div className={ styles.list }>
-				<h3 className={ styles.title }>{ title }</h3>
-				<ul className={ cn( isMinimal(), isCondensed(), isMono(), alignment() ) }>{ children }</ul>
+			<div className={styles.container}>
+				<h3 className={styles.title}>{title}</h3>
+				<ul
+					className={cn(
+						styles.list,
+						isMinimal(),
+						isCondensed(),
+						isMono(),
+						alignment(),
+					)}
+				>
+					{children}
+				</ul>
 			</div>
 		);
 	} else {
-		return <ul className={ cn( styles.list, isMinimal(), isCondensed(), isMono(), alignment(), className ) }>{ children }</ul>;
+		return (
+			<ul
+				className={cn(
+					styles.list,
+					isMinimal(),
+					isCondensed(),
+					isMono(),
+					alignment(),
+					className,
+				)}
+			>
+				{children}
+			</ul>
+		);
 	}
 }
