5394f2f7d25155893276ccaaa38ae452fe283070
Michael Arestad <marestad@gmail.com>
2021-06-21 21:34:47 -0600

    Added Storybook

--- components/avatar/avatar.js
+++ components/avatar/avatar.js
@@ -2,6 +2,15 @@ import styles from "./avatar.module.scss";
 
 export default function Avatar({ size = 160 }) {
 	return (
-		<img className={ styles.avatar } alt="Michael Arestad's face" width={ size } height={ size } src={ "https://www.gravatar.com/avatar/46e0f1b10224bb4e832ea9633acee2b8?s=" + ( size * 2 ) } />
+		<img
+			className={styles.avatar}
+			alt="Michael Arestad's face"
+			width={size}
+			height={size}
+			src={
+				"https://secure.gravatar.com/avatar/e8b4c8470f61ff15b9c98f7a1556c16b?s=" +
+				size * 2
+			}
+		/>
 	);
 }
