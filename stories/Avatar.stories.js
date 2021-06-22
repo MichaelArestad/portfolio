37ca63c78bdd11ec172847e35865ee86fe74648b
Michael Arestad <marestad@gmail.com>
2021-06-21 21:44:33 -0600

    Avatar Stories are complete

--- stories/Avatar.stories.js
+++ stories/Avatar.stories.js
@@ -3,13 +3,23 @@ import Avatar from "../components/avatar/avatar";
 export default {
 	title: "Portfolio/Avatar",
 	component: Avatar,
+	argTypes: {
+		size: {
+			control: { type: "range", min: 24, max: 160, step: 8 },
+		},
+	},
 };
 
 const Template = (args) => <Avatar {...args} />;
 
 export const Default = Template.bind({});
 
-// export const Small = Template.bind({});
-// Default.args = {
-//   size: 40,
-// };
+export const Medium = Template.bind({});
+Medium.args = {
+	size: 72,
+};
+
+export const Small = Template.bind({});
+Small.args = {
+	size: 40,
+};
